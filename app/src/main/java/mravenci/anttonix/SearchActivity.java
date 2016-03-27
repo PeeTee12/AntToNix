dd5d425852f1834f7896e4daa9748a1da0a319cb
PeeTee12 <petr.tobi@gmail.com>
2016-03-27 17:26:04 +0200

    Pokracovani hledani zarizeni, vyber zarizeni

--- app/src/main/java/mravenci/anttonix/SearchActivity.java
+++ app/src/main/java/mravenci/anttonix/SearchActivity.java
@@ -1,6 +1,8 @@
 package mravenci.anttonix;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,33 +11,62 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.dsi.ant.plugins.antplus.pcc.MultiDeviceSearch;
 import com.dsi.ant.plugins.antplus.pccbase.MultiDeviceSearch.MultiDeviceSearchResult;
 import com.dsi.ant.plugins.antplus.pcc.MultiDeviceSearch.SearchCallbacks;
 import com.dsi.ant.plugins.antplus.pcc.defines.DeviceType;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.util.ArrayList;
 
 
 public class SearchActivity extends AppCompatActivity {
 
     Context context;
     MultiDeviceSearch search;
-    Enum<DeviceType> deviceType;
-    MultiDeviceSearch.SearchCallbacks callbacks;
+    DeviceType deviceType;
+    SearchCallbacks callbacks;
+
+    ListView listView;
+    ArrayList<MultiDeviceSearchResult> foundDevice = new ArrayList<MultiDeviceSearchResult>();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         context = getApplicationContext();
-        deviceType = getAntDeviceType();
+        //deviceType = getAntDeviceType();
 
+
+        /**
+         * vypise nalezena zarizeni na obrazovce a po kliknuti prepne na obrazovku convert
+         * ulozi vybrane zarizeni
+         */
+        listView = (ListView) findViewById(R.id.listView_foundDevices);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(view.getContext(), ConvertActivity.class);
+                deviceType = listView.getItem(position);
+
+            }
+        });
+        /** spusti hledani zarizeni */
         search = new MultiDeviceSearch(this, deviceType, callbacks);
-        callbacks.onDeviceFound(getDeviceID());
 
 
         /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -46,6 +77,14 @@ public class SearchActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+    /** po vypnuti aplikace se hledani zastavi */
+    public void onDestroy(){
+        super.onDestroy();
+        search.close();
     }
 
     @Override
@@ -63,10 +102,50 @@ public class SearchActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Search Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://mravenci.anttonix/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Search Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://mravenci.anttonix/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
